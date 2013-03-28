88abfb012637c40b19a3a7bd36f98d4803891f3c
James <james.srn@gmail.com>
2013-03-28 11:13:25 +0000

    Create 04.py

---
+++ PythonChallenge/04.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# http://www.pythonchallenge.com/pc/def/linkedlist.php
+
+import urllib2
+
+base = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+nothing = 63579
+
+resp = "and the next nothing is "
+
+while "and the next nothing is " in resp:
+  conn = urllib2.urlopen(base + str(nothing))
+	resp = conn.read()
+	nothing = int(resp.replace('and the next nothing is ',''))
+	print nothing
+
+conn.close()
