18d705fe50ac46af1446a9e7f1d1f2be80fe3054
James Nutt <james.srn@gmail.com>
2013-08-02 22:57:56 +0100

    extra credit 1

--- Learning/LearnCTheHardWay/Lesson2/ex2.c
+++ Learning/LearnCTheHardWay/Lesson2/ex2.c
@@ -4,30 +4,22 @@ int main( int argc, char *argv[] )
 {
 	puts("Hello world.");
 
-	//  -- Extra Credit -- 
-	// 1) Open the ex1 file in your text editor and change or delete random parts.
-	// Try running it and see what happens.
-
-	// 2) Print out 5 more lines of text or something more complex than hello world.
 	puts("more");
 	puts("complex");
 	puts("than");
 	puts("hello");
 	puts("world");
 
-	/* 3) Run man 3 puts and read about this function and many others.
-	int fputc( int c, FILE *stream )
-		writes the character c, cast to an unsigned char, to stream
-	int fputs( const char *s, FILE *stream )
-		writes string s to stream, without its terminating null byte
-	int putc( int c, FILE *stream )
-		equivilent to fputc, except it can be implemented as a macro which
-		evaluates stream more than once
-	int putchar( int c )
-		equivilent to putc( c, stdout )
-	int puts( const char *s )
-		writes string s and a trailing newline to stdout
-	*/
+	// -- Extra Credit --
+	// 1) Create an all: ex1 target that will build ex1 with just the command
+	// make.
+	// 2) Read man make to find out more information on how to run it.
+	// 3) Read man cc to find out more information on what the flags -Wall and
+	// -g do.
+	// 4) Research Makefiles online and see if you can improve this one even
+	// more.
+	// 5) Find a Makefile in another C project and try to understand what it's
+	// doing.
 
 	return 0;
 }
