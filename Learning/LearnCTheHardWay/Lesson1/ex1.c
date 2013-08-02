76ad3dad5914832f604a61a6b9b0863dc0249006
James Nutt <james.srn@gmail.com>
2013-08-02 18:11:09 +0100

    lesson 1

--- Learning/LearnCTheHardWay/Lesson1/ex1.c
+++ Learning/LearnCTheHardWay/Lesson1/ex1.c
@@ -1,6 +1,19 @@
-int main(int argc, char *argv[])
+#include <stdio.h>
+
+int main( int argc, char *argv[] )
 {
     puts("Hello world.");
+
+    //  -- Extra Credit -- 
+    // 1) Open the ex1 file in your text editor and change or delete random parts.
+    // Try running it and see what happens.
+	// 2) Print out 5 more lines of text or something more complex than hello world.
+	puts("more");
+	puts("complex");
+	puts("than");
+	puts("hello");
+	puts("world");
+	// 3) Run man 3 puts and read about this function and many others.
 
     return 0;
 }
