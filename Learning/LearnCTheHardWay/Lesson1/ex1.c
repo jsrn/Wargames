e7f77d7ac45c83fbdff232ee95b2735a28dd3311
James Nutt <james.srn@gmail.com>
2013-08-02 18:11:59 +0100

    replaced spaces with tabs

--- Learning/LearnCTheHardWay/Lesson1/ex1.c
+++ Learning/LearnCTheHardWay/Lesson1/ex1.c
@@ -2,11 +2,11 @@
 
 int main( int argc, char *argv[] )
 {
-    puts("Hello world.");
+	puts("Hello world.");
 
-    //  -- Extra Credit -- 
-    // 1) Open the ex1 file in your text editor and change or delete random parts.
-    // Try running it and see what happens.
+	//  -- Extra Credit -- 
+	// 1) Open the ex1 file in your text editor and change or delete random parts.
+	// Try running it and see what happens.
 	// 2) Print out 5 more lines of text or something more complex than hello world.
 	puts("more");
 	puts("complex");
