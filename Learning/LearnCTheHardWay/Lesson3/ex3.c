cc2e90b2f23ea817df79646b3933cc30553caeff
James Nutt <james.srn@gmail.com>
2013-08-02 23:19:23 +0100

    ex3

---
+++ Learning/LearnCTheHardWay/Lesson3/ex3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int age = 10;
+	int height = 72;
+
+	printf( "I am %d years old.\n", age );
+	printf( "I am %d inches tall.\n", height );
+
+	return 0;
+
+	/*
+	-- External Research --
+	For this exercise I want you to go online and find out all of the printf 
+	escape codes and format sequences. Escape codes are \n or \t that let you 
+	print a newline or tab (respectively). Format sequences are the %s or %d 
+	that let you print a string or a integer. Find all of the ones available, 
+	how you can modify them, and what kind of "precisions" and widths you can 
+	do.
+	*/
+}
